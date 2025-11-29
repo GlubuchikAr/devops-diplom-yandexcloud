597175d29d59fa56d7a32465d24b4527cf9f17e0
GlubuchikAr <aeg6011@gmail.com>
2025-11-29 19:03:51 +0300

    Подготовка к8с кластера

--- 02/main.tf
+++ 02/main.tf
@@ -39,34 +39,74 @@ data "yandex_compute_image" "image-master" {
   family = var.instance_group.master.disk_image
 }
 
-# Создание группы ВМ master
-resource "yandex_compute_instance_group" "group-master" {
-  name                = var.instance_group.master.name
+# Создание ВМ master
+resource "yandex_compute_instance" "master" {
+  name                = "${var.instance_group.master.name}-${count.index + 1}"
+  count               = var.instance_group.master.fixed_scale
+  folder_id           = var.folder_id
+  service_account_id  = "${yandex_iam_service_account.groupvm-sa.id}"
+  depends_on          = [yandex_resourcemanager_folder_iam_member.group-editor]
+
+  platform_id = var.instance_group.master.platform_id
+  resources {
+    memory = var.instance_group.master.memory
+    cores  = var.instance_group.master.cores
+    core_fraction = var.instance_group.master.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.master.image_id
+      type     = var.instance_group.master.disk_type
+      size     = var.instance_group.master.disk_size
+    }
+  }
+
+  network_interface {
+    subnet_id   = yandex_vpc_subnet.subnet1.id
+    nat         = var.instance_group.master.nat
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  metadata = local.instance_metadata
+}
+
+# Определение образа для worker
+data "yandex_compute_image" "image-worker" {
+  family = var.instance_group.worker.disk_image
+}
+
+# Создание группы ВМ worker
+resource "yandex_compute_instance_group" "group-worker" {
+  name                = var.instance_group.worker.name
   folder_id           = var.folder_id
   service_account_id  = "${yandex_iam_service_account.groupvm-sa.id}"
   deletion_protection = "false"
   depends_on          = [yandex_resourcemanager_folder_iam_member.group-editor]
 
   instance_template {
-    platform_id = var.instance_group.master.platform_id
+    platform_id = var.instance_group.worker.platform_id
     resources {
-      memory = var.instance_group.master.memory
-      cores  = var.instance_group.master.cores
-      core_fraction = var.instance_group.master.core_fraction
+      memory = var.instance_group.worker.memory
+      cores  = var.instance_group.worker.cores
+      core_fraction = var.instance_group.worker.core_fraction
     }
 
   boot_disk {
     initialize_params {
-      image_id = data.yandex_compute_image.master.image_id
-      type     = var.instance_group.master.disk_type
-      size     = var.instance_group.masterp.disk_size
+      image_id = data.yandex_compute_image.worker.image_id
+      type     = var.instance_group.worker.disk_type
+      size     = var.instance_group.worker.disk_size
     }
   }
 
   network_interface {
     network_id         = "${yandex_vpc_network.diplom.id}"
     subnet_ids         = ["${yandex_vpc_subnet.subnet1.id}"]
-    nat = var.instance_group.masterp.nat
+    nat = var.instance_group.worker.nat
   }
 
   scheduling_policy {
@@ -78,7 +118,7 @@ resource "yandex_compute_instance_group" "group-master" {
 
   scale_policy {
     fixed_scale {
-      size = var.instance_group.lamp-group.fixed_scale
+      size = var.instance_group.worker.count
     }
   }
 
@@ -100,31 +140,6 @@ resource "yandex_compute_instance_group" "group-master" {
   }
 
   load_balancer {
-      target_group_name = "lamp-group"
+      target_group_name = "worker"
   }
 }
-
-# # Создание сетевого балансировщика
-# resource "yandex_lb_network_load_balancer" "lamp-balancer" {
-#   name = "lamp-network-balancer"
-
-#   listener {
-#     name = "http-listener"
-#     port = 80
-#     external_address_spec {
-#       ip_version = "ipv4"
-#     }
-#   }
-
-#   attached_target_group {
-#     target_group_id = yandex_compute_instance_group.group-vms.load_balancer[0].target_group_id
-
-#     healthcheck {
-#       name = "http-healthcheck"
-#       http_options {
-#         port = 80
-#         path = "/"
-#       }
-#     }
-#   }
-# }
